2855b505c12f39fc846b5872eca61940acd0ffae
Adam Jaros <adam@jaros.us>
2025-03-04 16:36:14 -0500

    Add initial data files and scripts for Pulse NMR

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# PHY451
+# PHY451 SS25
+## Pulse NMR
+automates a majority of the pulse nmr experiment
+- magnet current tuning for maximum echo amplitude
+- recording of amplitudes for sweeps of various pulse delays
---
+++ gpib.py
@@ -0,0 +1,40 @@
+import sys
+import serial
+
+
+class PrologixGPIB(object):
+	def __init__(self, devpath):
+		self.gpib = serial.Serial(devpath, baudrate=115200)
+		self.gpib.timeout = 1
+
+		# Query version to make sure it is live
+		self.gpib.write(b'++ver\n')
+		resp = self.gpib.readline()
+
+		if not resp.startswith(b'Prologix'):
+			raise RuntimeError('Cannot communicate with GPIB adapter at ' + devpath)
+
+		self('mode 1')
+
+	def __call__(self, command, response=False):
+		self.gpib.write((command + '\n').encode('us-ascii'))
+		if response:
+			out = self.gpib.readline().decode('us-ascii')
+			if len(out) == 0:
+				raise TimeoutError('Timeout on command %s' % command)
+			return out.strip()
+
+	def auto_rw(self, val=None):
+		if val is True:
+			self('++auto 1')
+		elif val is False:
+			self('++auto 0')
+		else:
+			return (self('++auto', True) == '1')
+
+	def addr(self, addr):
+		self('++addr %d' % addr)
+
+gpib = PrologixGPIB(sys.argv[1])
+gpib.addr(10)
+print(gpib('*idn?', True))
